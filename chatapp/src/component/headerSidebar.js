112b29e5f08a49fd352f9129849bee295112e3f9
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-01 14:13:49 -0400

    <router> moved to App

--- chatapp/src/component/headerSidebar.js
+++ chatapp/src/component/headerSidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './headerSidebar.css'
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const HeaderSidebar =(props) => {
    
@@ -12,16 +12,14 @@ const HeaderSidebar =(props) => {
             <aside className={props.menuclass}>
                 <div className="panel">
                     <ul className="options">
-                       <Router>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">search</span><p>Search</p></Link> </li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">home</span><p>Home</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">account_circle</span><p>Account</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">question_answer</span><p>Chat</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">notifications</span><p>Alerts</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">headset_mic</span><p>Music</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">call</span><p>Call</p></Link></li>
-                        <li><Link to={'#'} className="linkhover"><span className="material-icons">settings_applications</span><p>Settings</p></Link></li>
-                        </Router>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">search</span><p>Search</p></Link> </li>
+                        <li><Link to="/" className="linkhover"><span className="material-icons">home</span><p>Home</p></Link></li>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">account_circle</span><p>Account</p></Link></li>
+                        <li><Link to='/chat' className="linkhover"><span className="material-icons">question_answer</span><p>Chat</p></Link></li>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">notifications</span><p>Alerts</p></Link></li>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">headset_mic</span><p>Music</p></Link></li>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">call</span><p>Call</p></Link></li>
+                        <li><Link to='#' className="linkhover"><span className="material-icons">settings_applications</span><p>Settings</p></Link></li>
                     </ul>
                 </div>
                 <button className="toggle-others" onClick={ props.menu }><span>Menu</span></button>
