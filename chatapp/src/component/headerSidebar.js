4ac42dfc7b1a86febdee41811c59b6092246a591
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-25 02:05:40 -0400

    chaged link to value based on context variable

--- chatapp/src/component/headerSidebar.js
+++ chatapp/src/component/headerSidebar.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './headerSidebar.css'
 import { Link } from 'react-router-dom';
+import {userId} from '../App'
 
 const HeaderSidebar =(props) => {
-   
+    let userid = useContext(userId)
     return (
         <div className="layout">
             <header className="branding">
@@ -13,9 +14,9 @@ const HeaderSidebar =(props) => {
                 <div className="panel">
                     <ul className="options">
                         <li><Link to='#' className="linkhover"><span className="material-icons">search</span><p>Search</p></Link> </li>
-                        <li><Link to="/" className="linkhover"><span className="material-icons">home</span><p>Home</p></Link></li>
+                        <li><Link to={`/${userid}`} className="linkhover"><span className="material-icons">home</span><p>Home</p></Link></li>
                         <li><Link to='#' className="linkhover"><span className="material-icons">account_circle</span><p>Account</p></Link></li>
-                        <li><Link to='/chat' className="linkhover"><span className="material-icons">question_answer</span><p>Chat</p></Link></li>
+                        <li><Link to={`/${userid}/chat`} className="linkhover"><span className="material-icons">question_answer</span><p>Chat</p></Link></li>
                         <li><Link to='#' className="linkhover"><span className="material-icons">notifications</span><p>Alerts</p></Link></li>
                         <li><Link to='#' className="linkhover"><span className="material-icons">headset_mic</span><p>Music</p></Link></li>
                         <li><Link to='#' className="linkhover"><span className="material-icons">call</span><p>Call</p></Link></li>
