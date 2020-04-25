0d672e736beaf1f8ad75a47007f4fe6a07a6fcd4
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-25 17:59:12 -0400

    test of socket io in chat page with server

--- chatapp/src/component/chat.js
+++ chatapp/src/component/chat.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useEffect, useContext} from 'react';
 import './chat.css';
+import io from 'socket.io-client';
+import {userId} from '../App';
 
+
+let socket;
 const Chat = () => {
 
+    let userid = useContext(userId);
+    useEffect(()=>{
+        socket = io('localhost:5000');
+        console.log(socket);
 
+        socket.emit('join', {userid})
 
+        return(()=>{
+            socket.emit('disconnect');
+            socket.off();
+        })
+      },[userid])
+    
     return (
-        <h1>chat page</h1>
+       <p>chat page</p>
     )
 }
 
