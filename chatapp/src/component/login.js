1594281242fd76093eb4f051db316250c6c55157
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-25 17:58:28 -0400

    created login form

--- chatapp/src/component/login.js
+++ chatapp/src/component/login.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import './login.css'
 
 
 const Login = (props) => {
@@ -13,8 +14,21 @@ const Login = (props) => {
     
     return (
         <>
-            <input type="text" placeholder="userid" onChange={usertest} />
-            <Link to={`/${input}`} onClick={allset}>login</Link>
+        <div className="center">
+            <div className="signupBy">
+                <div className="input-form" >
+                    <div className="input-icons">  
+                        <i className="fa fa-envelope icon-signup"> </i> 
+                        <input className="input-field" type="text" placeholder="User Name" name="userid" onChange={usertest} />
+                        <i className="fas fa-unlock-alt icon-signup"></i>
+                        <input className="input-field" type="password" placeholder="Password" name="password" />
+                    </div> 
+                    
+                    <Link to={`/${input}`} onClick={allset}><button type="submit" className="button signup-btn"> Log In</button></Link>
+                    {/* <a href="#"><p className="forgotpass">Forgot password</p></a> */}
+                </div>
+            </div>
+        </div>
         </>
 )}
 
