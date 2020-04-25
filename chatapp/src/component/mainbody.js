0963ece0120073c9d8d0d313a838ad2d4c9a50f9
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-25 02:08:02 -0400

    use of context variable and route based on that variable

--- chatapp/src/component/mainbody.js
+++ chatapp/src/component/mainbody.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './main.css';
 import Home from './home';
 import Weather from './weather';
 import Chat from './chat';
+import {userId} from '../App'
 
 const Mainbody = (props) => {
+    
+    let userid = useContext(userId)
     const [size, setsize] = useState('mainsize');
     useEffect(() => { 
         if(props.menuclass === 'others open'){
@@ -18,12 +21,12 @@ const Mainbody = (props) => {
     return (
         <div className={size}>
             <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/forecast"  component={Weather} />
-                <Route path="/chat" component={Chat} />
-            </Switch>
+                <Route path={`/:${userid}`} exact component={Home}  />
+                <Route path={`/:${userid}/forecast`} component={Weather} />
+                <Route path={`/:${userid}/chat`} component={Chat} />
+            </Switch>    
         </div>
-        
+       
     )
 
 }
