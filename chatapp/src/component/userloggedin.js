7ae968daee61450c0d4368604792610eb9a6b7d6
bijaybdrkarki <bijay.jhupro@gmail.com>
2020-04-26 21:48:05 -0400

    initialize socket once user is logged and use same socket everytime within app

--- chatapp/src/component/userloggedin.js
+++ chatapp/src/component/userloggedin.js
@@ -1,14 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import HeaderSidebar from './headerSidebar';
 import Mainbody from './mainbody';
+import * as io from 'socket.io-client';
+import {userId} from '../App';
 
 
-
+let socket;
 
 const UserLoggedin = () => { 
     
     const [menuclass, setmenuclass] = useState('others'); 
+    let userid = useContext(userId);
     
+
+    useEffect(()=>{
+      socket = io('localhost:5000');
+      console.log(socket);
+      return(()=>{
+          socket.emit('disconnect');
+          socket.off();
+      })
+  },[userid])
     
     function menu(){
       if (menuclass === 'others')
@@ -21,10 +33,11 @@ const UserLoggedin = () => {
       }
     }
     return (
-    <> 
-       <HeaderSidebar menuclass={menuclass} menu={menu} />
-       <Mainbody menuclass={menuclass} />
-    </>
+     <>
+        <HeaderSidebar menuclass={menuclass} menu={menu} />
+        <Mainbody menuclass={menuclass} socket={socket} />
+      </>
 )}
 
 export default UserLoggedin;
+
